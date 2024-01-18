d17ee7a41cb3ae7a920dc147551eb23671a22473
Lesson-Gear <155381875+Lesson-Gear@users.noreply.github.com>
2024-01-18 08:27:59 +0100

    Update darkmode.js

--- Central/darkmode.js
+++ Central/darkmode.js
@@ -1,1 +1,14 @@
+        document.addEventListener('DOMContentLoaded', function () {
+            const darkModeToggle = document.getElementById('darkModeToggle');
 
+            darkModeToggle.addEventListener('change', function () {
+                document.documentElement.classList.toggle('dark-mode', darkModeToggle.checked);
+                localStorage.setItem('darkMode', darkModeToggle.checked ? 'enabled' : 'disabled');
+            });
+
+            // Check the user's preference from localStorage
+            if (localStorage.getItem('darkMode') === 'enabled') {
+                darkModeToggle.checked = true;
+                document.documentElement.classList.add('dark-mode');
+            }
+        });
